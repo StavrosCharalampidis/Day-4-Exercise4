92d01600a801f5071397d1e418361632464d7307
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-02 13:58:21 +0200

    Variables Complete

---
+++ ex4.py
@@ -0,0 +1,18 @@
+# A long Comment
+'''
+# in this example he has an Error becuase the some_Name is not write above the Variable nameErr 
+someName = 5
+nameErr = some_Name + 1
+print(nameErr) # error NameError
+
+'''
+
+# Variables i j x
+i = 22 - 12
+j = 12 * 2
+x = 12 % 12
+
+
+print("i = ", i, ",j = ", j ,",x = ", x) 
+
+
